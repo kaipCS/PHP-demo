9d323c0a7c5a7391bfb804c33828560ac5a8e618
kaipCS <parker.ceh@oundleschool.org.uk>
2024-10-10 12:47:57 +0100

    push

---
+++ adder.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Javascript test</title>
+</head>
+<body>
+ 
+<form action="welcome.php" method="POST">
+1st number: <input type="text" name="num1"><br>
+2nd number: <input type="text" name="num2"><br>
+<input type="submit">
+</form>
+
+  
+</body>
+</html> 
---
+++ dice.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> php demo</title>
+</head>
+<body>
+
+<?php
+    echo("oooooooooooo<br>");
+    echo("<pre>o            o</pre><br>");
+    echo("<pre>o   #    #   o</pre><br>");
+    echo("<pre>o     #      o</pre><br>");
+    echo("<pre>o   #    #   o</pre><br>");
+    echo("<pre>o            o</pre><br>");
+    echo("ooooooooooo");
+?>
+
+</body>
+</html> 
