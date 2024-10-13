fc307c36ea17d2411c89153598c22209876cf05b
kaipCS <parker.ceh@oundleschool.org.uk>
2024-10-13 14:37:52 +0100

    sync

--- adder.php
+++ adder.php
@@ -6,8 +6,8 @@
 <body>
  
 <form action="welcome.php" method="POST">
-1st number: <input type="text" name="num1"><br>
-2nd number: <input type="text" name="num2"><br>
+1st number: <input type="number" name="num1"><br>
+2nd number: <input type="number" name="num2"><br>
 <input type="submit">
 </form>
 
---
+++ welcome.php
@@ -0,0 +1,6 @@
+<<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$sum = $num1 + $num2;
+echo"The sum of the two numbers is: " . [$sum];
+?>
