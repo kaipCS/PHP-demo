8414abba21d297fabf63fbea43939763aa48f7b8
kaipCS <parker.ceh@oundleschool.org.uk>
2024-10-15 09:18:47 +0100

    done

---
+++ textdice.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> PHP test</title>
+</head>
+<body>
+ 
+ 
+<form action="welcome6.php">
+<input type="submit" value="Random Number">
+</form>
+
+</body>
+</html> 
---
+++ welcome6.php
@@ -0,0 +1,19 @@
+<?php
+
+$num = (rand(1,6));
+
+if ($num == 1) {
+    echo "One";
+    } elseif ($num == 2) {
+    echo "Two";
+    }elseif ($num == 3) {
+    echo "Three";
+    } elseif ($num == 4) {
+    echo "Four";
+    } elseif ($num == 5) {
+    echo "Five";
+    }
+    else {
+    echo "Six";
+    }
+?>
